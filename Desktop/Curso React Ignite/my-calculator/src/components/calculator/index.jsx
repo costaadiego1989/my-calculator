b444e17cdf6293e7fba33e0daef3c499039fa134
diego <diego.oliveira@fcamara.com.br>
2022-06-01 17:11:00 -0300

    feat: adicionando cores aos botões.

--- Desktop/Curso React Ignite/my-calculator/src/components/calculator/index.jsx
+++ Desktop/Curso React Ignite/my-calculator/src/components/calculator/index.jsx
@@ -7,28 +7,28 @@ export const Calculator = () => {
       <div className="containerCalculator">
         <div className="screenCalculator">{0}</div>
         <div className="contentCalculator">
-          <button>C</button>
-          <button>+/-</button>
-          <button value="%">%</button>
-          <button value="+">+/-</button>
+          <button className="darkGrey">C</button>
+          <button className="darkGrey">+/-</button>
+          <button className="darkGrey" value="%">%</button>
+          <button className="orange" value="+">+/-</button>
           <button value={1}>1</button>
           <button value={2}>2</button>
           <button value={3}>3</button>
-          <button value="-">-</button>
+          <button className="orange" value="-">-</button>
           <button value={4}>4</button>
           <button value={5}>5</button>
           <button value={6}>6</button>
-          <button value="*">x</button>
+          <button className="orange" value="*">x</button>
           <button value={7}>7</button>
           <button value={8}>8</button>
           <button value={9}>9</button>
-          <button value="/">/</button>
+          <button className="orange" value="/">/</button>
           <button value={0}>0</button>
           <button className="darkGrey" style={{ visibility: "hidden" }}>
             del
           </button>
           <button value=".">,</button>
-          <button>=</button>
+          <button className="orange">=</button>
         </div>
       </div>
     </div>
