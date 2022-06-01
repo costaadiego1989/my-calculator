e13a3f1f3246c4890871d3c3fff03b33080a7a0f
diego <diego.oliveira@fcamara.com.br>
2022-06-01 17:29:41 -0300

    feat: criado funcao handleOperator que pega o operador matematico.

--- Desktop/Curso React Ignite/my-calculator/src/components/calculator/index.jsx
+++ Desktop/Curso React Ignite/my-calculator/src/components/calculator/index.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 export const Calculator = () => {
   const [primaryValue, setPrimaryValue] = useState(0);
+  const [operator, setOperator] = useState("");
 
   const handleChangeValue = (e) => {
     if (primaryValue === 0) {
@@ -26,6 +27,10 @@ export const Calculator = () => {
     setPrimaryValue(primaryValue / 100);
   }
 
+  const handleOperator = (e) => {
+    setOperator(e);
+  }
+
   return (
     <div className="container">
       <div className="containerCalculator">
@@ -34,19 +39,19 @@ export const Calculator = () => {
           <button className="darkGrey" onClick={handleDelete}>C</button>
           <button className="darkGrey" onClick={handleChangePlusAndMinus}>+/-</button>
           <button className="darkGrey" onClick={handlePercentage} value="%">%</button>
-          <button className="orange" value="+">+</button>
+          <button className="orange" value="+" onClick={e => handleOperator(e.target.value)}>+</button>
           <button value={1} onClick={e => handleChangeValue(e.target.value)}>1</button>
           <button value={2} onClick={e => handleChangeValue(e.target.value)}>2</button>
           <button value={3} onClick={e => handleChangeValue(e.target.value)}>3</button>
-          <button className="orange" value="-">-</button>
+          <button className="orange" value="-" onClick={e => handleOperator(e.target.value)}>-</button>
           <button value={4} onClick={e => handleChangeValue(e.target.value)}>4</button>
           <button value={5} onClick={e => handleChangeValue(e.target.value)}>5</button>
           <button value={6} onClick={e => handleChangeValue(e.target.value)}>6</button>
-          <button className="orange" value="*">x</button>
+          <button className="orange" value="*" onClick={e => handleOperator(e.target.value)}>x</button>
           <button value={7} onClick={e => handleChangeValue(e.target.value)}>7</button>
           <button value={8} onClick={e => handleChangeValue(e.target.value)}>8</button>
           <button value={9} onClick={e => handleChangeValue(e.target.value)}>9</button>
-          <button className="orange" value="/">/</button>
+          <button className="orange" value="/" onClick={e => handleOperator(e.target.value)}>/</button>
           <button value={0} onClick={e => handleChangeValue(e.target.value)}>0</button>
           <button className="darkGrey" style={{ visibility: "hidden" }}>del</button>
           <button value="." onClick={e => handleChangeValue(e.target.value)}>,</button>
